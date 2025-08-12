ec737f76ad94c1c15ddb454a636551dcb825ef38
bfranks56 <bryant.franks@gmail.com>
2025-08-12 09:23:00 -0400

    remove nx-welcome screen

--- apps/personal-site/src/app/app.routes.ts
+++ apps/personal-site/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' },
+];
