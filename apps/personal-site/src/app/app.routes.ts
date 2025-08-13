677e02c5c8fccc392ff78a33396b541bad8d9c93
bfranks56 <bryant.franks@gmail.com>
2025-08-13 13:12:43 -0400

    add lazy loading

--- apps/personal-site/src/app/app.routes.ts
+++ apps/personal-site/src/app/app.routes.ts
@@ -1,22 +1,46 @@
 import { Route } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { DefaultPageComponent } from './pages/default-page/default-page.component';
 import { ABOUT_CONTENT, HOME_CONTENT } from './content';
 
 export const appRoutes: Route[] = [
   {
     path: '',
-    component: DefaultPageComponent,
+    loadComponent: () =>
+      import('./pages/default-page/default-page.component').then(
+        c => c.DefaultPageComponent
+      ),
     data: { pageContent: HOME_CONTENT },
   },
   {
     path: 'about',
-    component: DefaultPageComponent,
+    loadComponent: () =>
+      import('./pages/default-page/default-page.component').then(
+        c => c.DefaultPageComponent
+      ),
     data: { pageContent: ABOUT_CONTENT },
   },
-  { path: 'contact', component: ContactComponent },
-  { path: 'projects', component: ProjectsComponent },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(c => c.ContactComponent),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./pages/projects/projects.component').then(
+        c => c.ProjectsComponent
+      ),
+  },
+  // {
+  //   path: '',
+  //   component: DefaultPageComponent,
+  //   data: { pageContent: HOME_CONTENT },
+  // },
+  // {
+  //   path: 'about',
+  //   component: DefaultPageComponent,
+  //   data: { pageContent: ABOUT_CONTENT },
+  // },
+  // { path: 'contact', component: ContactComponent },
+  // { path: 'projects', component: ProjectsComponent },
   { path: '**', redirectTo: '' },
 ];
